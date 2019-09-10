a6917f992b9510a7e5c973fa4b2e9e9c1fb05b2c
Jan Morten Kivi <janmortenk@users.noreplay.github.com>
2019-09-10 11:09:10 +0300

    Esimene üleslaadimine

---
+++ 02tund/myindex.php
@@ -0,0 +1,19 @@
+<?php
+  //echo "Hei!";
+  phpversion();
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>Jan Morten programmeerib veebi</title>
+
+</head>
+<body>
+  <h1>Jan Morten veebiprogrammeerimine</h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <hr>
+  <p>PHP serveri konfiguratsiooni infot näeb <a href="serverinfo.php">siin</a>.</P>
+  <p>Minu esimene programmeeritud leht on <a href="testpage.php"> siin</a>.<p>
+</body>
+</html>
---
+++ 02tund/testpage.php
@@ -0,0 +1,21 @@
+<?php
+  $userName = "Jan Morten Kivi";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+     echo $userName;
+  ?>
+   programmeerib veebi</title>
+
+</head>
+<body>
+  <?php
+    echo "<h1>" .$userName ." veebiprogrammeerimine</h1>";
+  ?>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+</body>
+</html>
