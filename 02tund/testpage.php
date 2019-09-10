cbd2183041eb655ee0b46040013efa6b282ad5a1
Jan Morten Kivi <janmortenk@users.noreplay.github.com>
2019-09-10 11:34:13 +0300

    lisatud kellaaja kontrollimine

--- 02tund/testpage.php
+++ 02tund/testpage.php
@@ -1,5 +1,12 @@
 <?php
   $userName = "Jan Morten Kivi";
+  $fullTimeNow = date("d.m.Y H:i:s");
+  $hourNow = date("H");
+  $partOfDay = "hägune aeg";
+  
+  if($hourNow < 8) {
+	  $partOfDay = "hommik";
+  }  
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -17,5 +24,9 @@
     echo "<h1>" .$userName ." veebiprogrammeerimine</h1>";
   ?>
   <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <hr>
+  <?php
+  echo "<p>Lehe avamise hetkel oli aeg: " .$fullTimeNow .", ".$partOfDay .".</p>";
+  ?>
 </body>
 </html>
